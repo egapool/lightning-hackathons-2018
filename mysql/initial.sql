e2e257c7fc304efdfd5289efd5bed47eaaa427c2
egapool <egpersent@gmail.com>
2018-09-30 11:04:41 +0900

    make init sql

--- mysql/initial.sql
+++ mysql/initial.sql
@@ -1,5 +1,39 @@
-CREATE TABLE `table` (
+CREATE TABLE `elections` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `some_column` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `description` text,
+  `vote` int(11) DEFAULT NULL,
+  `open` datetime DEFAULT NULL,
+  `close` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `invoices` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `election_id` int(11) DEFAULT NULL,
+  `subject_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `paid` tinyint(1) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `subjects` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `election_id` int(11) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+CREATE TABLE `users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
